2d9bcae01174e8fad899186b23b0caf2146d4083
rafaellkl11 <rafaelhenriqueif>
2024-08-10 12:29:55 -0300

    1°commit

--- rafael01.java
+++ rafael01.java
@@ -1,1 +1,13 @@
-m
+/*1. Implemente um algoritmo que imprima todos os números inteiros do intervalo fechado de 1 a 100.
+*/
+public class rafael01{
+    public static void main(String[] args) {
+        int n;
+
+        System.out.println("Algoritmo que imprime os números de 0 a 100");
+        
+        for (n=1; n!=101; n++){
+            System.out.println(n);
+        }
+    }
+}
