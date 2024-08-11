a18d1d19027e117b8087c47eb9cd985f59d28c58
rafaellkl11 <rafaelhenriqueif>
2024-08-11 15:03:10 -0300

    3°commit

---
+++ rafael03.java
@@ -0,0 +1,11 @@
+/*3. Escreva um algoritmo que imprima os números de 100 à 1 (ordem decrescente).*/
+public class rafael03 {
+    public static void main(String[] args) {
+        
+        System.out.println("Números de 100 a 1:");
+
+        for (int n = 100; n >= 1; n = n - 1 ){
+            System.out.print(n+", ");
+        }
+    }
+}
