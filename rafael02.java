e8d16a1f4c4b8c6097b972c5080182e8ba98901c
rafaellkl11 <rafaelhenriqueif>
2024-08-11 14:58:41 -0300

    2°commit

---
+++ rafael02.java
@@ -0,0 +1,15 @@
+/*2. Escreva um programa que imprima os números pares de 0 à 50. Utilize a estrutura for.*/
+
+public class rafael02{
+    public static void main(String[] args){
+
+
+        System.out.println("2. Números pares de 0 a 50:");
+
+
+        for (int n = 0; n <= 50; n = n + 2 ) {
+            System.out.print(n+(", "));
+        }
+
+    }
+}
