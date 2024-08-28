14c6031250066385be597ef94ced2cdf4b4654f0
rafaellkl11 <rafaelhenriqueif>
2024-08-27 21:09:36 -0300

    24°commit

--- rafael26.java
+++ rafael26.java
@@ -6,37 +6,39 @@ import java.util.Scanner;
 public class rafael26{
    public static void main(String[] args){
       Scanner teclado = new Scanner (System.in);
-      double xGraus, xRad, sen, fat, fatAux;
+      double xGraus, xRad, sen, fat, expo, fatAux;
      
-      fat = 0;
+      fat = 1;
       sen = 0;
+      expo = 0;
      
-      System.out.println("16. Programa que calcula o seno de X");
+      System.out.println("26. Programa que calcula o seno de X");
      
       System.out.print("Digite o valor de X: ");
       xGraus = teclado.nextDouble();
+
+      xRad = xGraus * 3.14 / 180;
      
       for (int i = 1; i <= 15; i++) {
-         System.out.println(i);
          fat = 2 * i - 1;
+         expo = 2 * i - 1;
          fatAux = fat;
-         System.out.println(fat);
-                 
-         for (double j = fatAux; j >= 0; j--){
+
+         for (double j = 1; j <= fatAux-1; j++){
             fat = fat * j;
-            System.out.println(fat);
-           
-           
-         }
-         
-         
+         }        
+
          if ((i%2) == 1){
-            sen = sen + Math.pow(xGraus,3)/fat;
+            sen = sen + Math.pow(xRad,expo)/fat;
          }
          else {
+            sen = sen - Math.pow(xRad,expo)/fat;
+         }   
 
-         }        
       }
-      System.out.println("sen"+sen);        
+      System.out.println("Resultado: ");
+      System.out.printf("Valor do seno: %.2f",sen);  
+      
+      teclado.close();
    }
 }
